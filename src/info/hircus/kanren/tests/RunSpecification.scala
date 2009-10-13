a1c4bdb1e5d8e6838121e787cd57aca8618d1f26
Michel Alexandre Salim <salimma@fedoraproject.org>
2009-10-13 01:33:41 -0400

    Added tests for conde and member_o (not working yet)

--- src/info/hircus/kanren/tests/RunSpecification.scala
+++ src/info/hircus/kanren/tests/RunSpecification.scala
@@ -26,6 +26,29 @@ object RunSpecification extends Properties("Run") {
     run(-1, w)(all(mkEqual(v, n), mkEqual(w, m))) == m
   }
 
+  /* test-check "conde extensive"
+   *   (run* (x)
+   *     (fresh (y)
+   *       (conde
+   *         ((conde
+   *            ((== y 1))
+   *            ((== y 11))))
+   *         ((conde
+   *            ((== y 2))
+   *            (fail)
+   *            ((== y 3)))))
+   *       (conde
+   *         ((== x y))
+   *         ((== x 100)))))
+   *   '(1 100 11 100 2 100 3 100))
+   */
+  property("conde") = run(-1, v) { s: Subst =>
+    val y = make_var('y)
+    all(cond_e(List(cond_e(List(mkEqual(y, 1) _, mkEqual(y, 11) _))),
+	       List(cond_e(List(mkEqual(y, 2) _, fail _, mkEqual(y, 3) _)))),
+	cond_e(List(mkEqual(v, y) _), List(mkEqual(v, 100) _)))(s)
+  } == List(1, 100, 11, 100, 2, 100, 3, 100)
+
   property("null") = forAll { n: Int =>
     val x = make_var('x)
     if (n <= 0) true
@@ -49,6 +72,8 @@ object RunSpecification extends Properties("Run") {
       val res = run(n, ls)(list_o(('a, ('b, ('c, ls)))))
       res.length == n
     }
-    
   }
+
+  property("member0") = run(-1, v)(member_o(v, Nil)) == Nil
+  property("member1") = forAll { n: Int => run(-1, v)(member_o(v, List(n))) == List(n) }
 }
