e0ae49591ad2d0b5ba2905261f36e2563d3f046c
Michel Alexandre Salim <salimma@fedoraproject.org>
2009-10-16 18:05:14 -0400

    - Make mk-scheme use full Scheme MK prelude file - Scala Kanren shell now loads the new prelude, and starts with   variables x,y,z defined

--- src/shell.scala
+++ src/shell.scala
@@ -1,3 +1,7 @@
 import info.hircus.kanren.MiniKanren._
-import info.hircus.kanren.MKLib._
+import info.hircus.kanren.Prelude._
 import info.hircus.kanren.MKMath._
+
+var x = make_var('x)
+var y = make_var('y)
+var z = make_var('z)
