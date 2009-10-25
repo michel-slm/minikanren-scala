464f26d2b136c30e33917af18fbeee35f47b4943
Michel Alexandre Salim <salimma@fedoraproject.org>
2009-10-25 15:31:44 -0400

    Add ntimes timer for benchmarking

--- src/shell.scala
+++ src/shell.scala
@@ -12,6 +12,16 @@ def time(block: => Any) = {
   val start = System currentTimeMillis ()
   val res   = block
   val stop  = System currentTimeMillis ()
-  println("Elapsed: " + (stop-start) + " ms")
-  res
+  ((stop-start), res)
+}
+
+def ntimes(n: Int, block: => Any) = {
+  // folding a list of Longs is cumbersome
+  def adder(x:Long,y:Long) = x+y
+  val zero : Long = 0
+
+  // compute only once!
+  val res = (for (i <- 0 until n) yield (time(block) _1)).toList
+  println("Elapsed times: " + res)
+  println("Avg: " + (res.foldLeft(zero)(adder) / n))
 }
